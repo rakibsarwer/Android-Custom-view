8cd4a67eac05e6024d016a7d778358702ff1ac95
rakibsarwer <rakibsarwar3@gmail.com>
2018-07-22 09:32:23 +0600

    initial commit . Skill development

---
+++ app/src/main/java/com/example/lenovo/android_theme_style_c8/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.android_theme_style_c8;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
